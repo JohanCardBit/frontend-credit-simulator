7c107c1c02597abfa000dcdca5ae709ea7cd4084
JohanCard <johancardgit@gmail.com>
2025-11-24 17:10:05 -0500

    modificaciones en home.

--- src/app/components/public/home/home.ts
+++ src/app/components/public/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Inicio } from "./inicio/inicio";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, Inicio],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
