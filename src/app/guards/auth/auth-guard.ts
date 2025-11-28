b3c55a4cd1b86e0a1c5aa45088eb07952bc09c5d
JohanCard <johancardgit@gmail.com>
2025-11-28 03:06:24 -0500

    auth y block OK

---
+++ src/app/guards/auth/auth-guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const cookieService = inject(CookieService);
+
+  const token = cookieService.get('token');
+
+  if (!token) {
+    // No hay token → redirige al login
+    router.navigate(['/login']);
+    return false;
+  }
+
+  return true;
+};
